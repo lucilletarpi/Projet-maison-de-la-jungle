2f95756546b8e17aa19d39d9d3cd4acc2378672f
lucilletarpi <lucille-tarpi@hotmail.fr>
2023-01-08 16:14:24 +0100

    projet terminé reflexion a d'autres fonctionnalités

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,34 +4,30 @@ import '../styles/Footer.css'
 function Footer() {
 	const [inputValue, setInputValue] = useState('')
 
-  function handleInput(e){
-    setInputValue(e.target.value)
-    
-  }
+	function handleInput(e) {
+		setInputValue(e.target.value)
+	}
 
-  function handleBlur(){
-    if(!inputValue.includes('@')){
-      alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
-    }
-  }
+	function handleBlur() {
+		if (!inputValue.includes('@')) {
+			alert("Attention, il n'y a pas d'@, ceci n'est pas une adresse valide 😥")
+		}
+	}
 
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
 				Pour les passionné·e·s de plantes 🌿🌱🌵
 			</div>
-			  <p className='lmj-footer-elem'>Laissez-nous votre mail :
-        </p>
-          <input
-          placeholder='Votre E-mail ici' 
-          onChange={handleInput} 
-          value={inputValue} 
-          onBlur={handleBlur} 
-          />
-          {console.log(setInputValue)}
+			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
+			<input
+				placeholder='Entrez votre mail'
+				onChange={handleInput}
+				value={inputValue}
+				onBlur={handleBlur}
+			/>
 		</footer>
 	)
-  
 }
 
 export default Footer
